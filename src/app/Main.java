921348ef3a3d76ee2343513a2258efef297eb516
Ihor Robeiko <rid@ukr.net>
2022-12-29 22:24:10 +0200

    Add app version

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,7 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("My app for Users!");
+        System.out.println("Version 1.0");
         double miles = 15;
         double kms = 26;
         double kmsRes = convertMilesToKms(miles);
