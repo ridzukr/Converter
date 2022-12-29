2f6b62cbfd78aeab325eeb165b4ef04076a083c5
Ihor Robeiko <rid@ukr.net>
2022-12-29 22:17:53 +0200

    Change app info: short text

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,7 +2,7 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My git app. Supper!");
+        System.out.println("Supper app!");
         double miles = 15;
         double kms = 26;
         double kmsRes = convertMilesToKms(miles);
