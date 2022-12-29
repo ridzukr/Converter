e1f0b169763a5022a8844a47bc52ee22a47bcf6b
Ihor Robeiko <rid@ukr.net>
2022-12-29 21:22:42 +0200

    Add converter Kms

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,9 +7,15 @@ public class Main {
         double kms = 26;
         double kmsRes = convertMilesToKms(miles);
         System.out.printf("%1$.4f kms in %2$.1f miles.\n", kmsRes, miles);
+        double milesRes = convertKmsToMiles(kms);
+        System.out.printf("%1$.4f miles in %2$.1f kms.\n", milesRes, kms );
     }
 
     private static double convertMilesToKms(double miles){
      return miles * 1.60934;
+    }
+
+    private static double convertKmsToMiles(double kms) {
+        return kms / 1.60934;
     }
 }
